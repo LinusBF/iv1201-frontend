47ab4d339f418661703d21913ffd98e5e825059f
LinusBF <contact@linusbf.com>
2020-01-31 11:20:55 +0100

    Added basic backend calling test and module

--- index.js
+++ index.js
@@ -3,11 +3,29 @@
 const express = require('express');
 const app = express();
 
+const backendConnection = require('src/backendConnection');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
   res.send(`Hello ${target}!`);
+});
+
+app.get('/server-side-call', (req, res) => {
+  backendConnection
+    .post('/', {data: 'this is front end talking'})
+    .then(response => {
+      console.info(`Received ${response} from backend`);
+      res.status(200).send(response);
+    })
+    .catch(err => {
+      console.error(`Request to backend failed with error ${err}`);
+      res.status(500).send('Please check the logs!');
+    });
 });
 
 const port = process.env.PORT || 8080;
---
+++ test/backendConnection.test.js
@@ -0,0 +1,44 @@
+'use strict';
+const chai = require('chai');
+const expect = chai.expect;
+const utils = require('@google-cloud/nodejs-repo-tools');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const request = require('request-promise-native');
+
+const backendConnection = require('../src/backendConnection');
+
+chai.use(sinonChai);
+
+beforeEach(utils.stubConsole);
+afterEach(utils.restoreConsole);
+
+describe('Backend Connection Module', function() {
+  afterEach(function() {
+    sinon.restore();
+  });
+
+  it('should make a request with a authentication token', function() {
+    const path = '/hello';
+    const options = {data: {test: 'TEST'}};
+
+    const fakeAuthCall = sinon.fake.resolves('TEST_TOKEN');
+    const fakePostCall = sinon.fake.returns({
+      auth: () => {
+        return {data: 'THIS IS A TEST', statusCode: 200};
+      },
+    });
+
+    sinon.replace(request, 'get', fakeAuthCall);
+    sinon.replace(request, 'post', fakePostCall);
+
+    return backendConnection
+      .post(path, options)
+      .then(response => {
+        expect(response.statusCode).to.be.eq(200);
+      })
+      .catch(err => {
+        expect(err).to.be.undefined;
+      });
+  });
+});
