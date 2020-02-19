037589c2ef4176989590806ec0b864e4295c4eff
Melker Mossberg <melker.mossberg@gmail.com>
2020-02-19 19:06:30 +0100

    added fakedatasource to the list and changed CSS to be aligned in columns instead of justify-even

--- client/components/applications/Applications.js
+++ client/components/applications/Applications.js
@@ -5,12 +5,15 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import {ListGroupItem} from 'react-bootstrap';
 import './Applications.css';
 import clown from '../../Images/flat.svg';
+import data from './fakeApplicationData'
 
 class Applications extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: Array.from({length: 20}),
+      loadedSoFar: 20,
+      //items: Array.from({length: 20}),
+      items: data.apps.slice(0,20)
     };
     this.fetchMoreData = this.fetchMoreData.bind(this);
   }
@@ -20,9 +23,13 @@ class Applications extends Component {
     // 20 more records in 1.5 secs
     setTimeout(() => {
       this.setState({
-        items: this.state.items.concat(Array.from({length: 20})),
+        items: this.state.items.concat(data.apps.slice(0,this.state.loadedSoFar+20))
       });
     }, 1500);
+  }
+
+  calcAge(ssn){
+    return 25;
   }
 
   render() {
@@ -38,16 +45,16 @@ class Applications extends Component {
           </div>
         </div>
 
-        <ListGroup className={'container justify-content-md-center'} id={'listGroup'}>
+        <ListGroup id={'listGroup'} className={'container justify-content-md-center'} >
           <ListGroupItem className={'list-group-item list-group-item-action active flex-column align-items-start'} id={'listGroup-header'}>
             <div className='d-flex w-100 justify-content-between' >
-              <div style={{marginRight: '-0.2rem'}}><p>Received</p></div>
-              <div style={{marginRight: '3.2rem'}}><p>Name</p></div>
-              <div style={{marginRight: '-2.4rem'}}><p>Age</p></div>
-              <div style={{marginRight: '-2.2rem'}}><p>Skills</p></div>
-              <div style={{marginRight: '-3rem'}}><p>Days</p></div>
-              <div style={{marginRight: '2rem'}}><p>Start</p></div>
-              <div style={{marginRight: '1.6rem'}}><p>Decision</p></div>
+              <div className={'col-2-sm'} style={{marginRight: '4rem'}}><p>Received</p></div>
+              <div className={'col-3'}><p>Name</p></div>
+              <div className={'col'}><p>Age</p></div>
+              <div className={'col'}><p>Skills</p></div>
+              <div className={'col'}><p>Days</p></div>
+              <div className={'col'}><p>Start</p></div>
+              <div className={'col'}><p>Decision</p></div>
             </div>
           </ListGroupItem>
           <InfiniteScroll
@@ -58,14 +65,14 @@ class Applications extends Component {
           >
             {this.state.items.map((i, index) => (
               <ListGroupItem className={'list-group-item list-group-item-action flex-column align-items-start'} key={index}>
-                <div className='d-flex w-100 justify-content-between'>
-                  <p>2020-09-32</p>
-                  <p>Melker Mossberg</p>
-                  <p>19</p>
-                  <p>20</p>
-                  <p>30</p>
-                  <p>2020-09-32</p>
-                  <div className='alert alert-warning' role='alert'>No decision</div>
+                <div className='d-flex w-100'>
+                  <p className={'col-2-sm'} style={{marginRight: '3rem'}}>{this.state.items[index].applyDate}</p>
+                  <p className={'col-3'}>{this.state.items[index].firstName} {this.state.items[index].lastName}</p>
+                  <p className={'col'}>{this.calcAge(this.state.items[index].ssn)}</p>
+                  <p className={'col'}>{this.state.items[index].expertise.length}</p>
+                  <p className={'col'}>{this.state.items[index].available.length}</p>
+                  <p className={'col-2-sm'} style={{marginRight: '4rem'}}>{this.state.items[index].available[0]}</p>
+                  <div className='alert alert-warning col text-center' role='alert'>{this.state.items[index].approved.toString()}</div>
                 </div>
               </ListGroupItem>
             ))}
