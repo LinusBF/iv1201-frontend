20c82b42c10e7935902e528b3d7a89b79b0f2cb3
Melker Mossberg <melker.mossberg@gmail.com>
2020-02-24 10:27:59 +0100

    enabled eslint again

--- client/components/applicationForm/ApplicationForm.js
+++ client/components/applicationForm/ApplicationForm.js
@@ -1,17 +1,16 @@
 /*eslint-disable*/
-import React, {Component, createElement} from 'react';
+import React, {Component} from 'react';
 import './ApplicationForm.css';
 import data from './fakeSingleApplicationData';
 import {Form} from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import {forEach} from 'react-bootstrap/cjs/ElementChildren';
 
 class ApplicationForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       expertises: data.expertises,
-      expSelections: ['Select an expertise']
+      expSelections: ['Select an expertise'],
     };
     this.createListOptions = this.createListOptions.bind(this);
     this.createLists = this.createLists.bind(this);
@@ -20,46 +19,57 @@ class ApplicationForm extends Component {
   }
 
   // Expertise-picker
-  createLists(array){
+  createLists(state) {
+    const expertiseList = state.expertises;
+    const selections = state.expSelections;
     const lists = [];
-    array.forEach((key, index) => {
-      lists.push(<Form.Control name={'expertise'} onChange={()=>{this.addExpertiseRow(key, index)}} as="select">
-        {this.createListOptions()}
-      </Form.Control>)
+    selections.forEach((key, index) => {
+      lists.push(
+        <Form.Control
+          name={'expertise'}
+          onChange={() => {
+            this.addExpertiseRow(key, index);
+          }}
+          as="select"
+        >
+          {this.createListOptions(expertiseList)}
+        </Form.Control>
+      );
     });
     return lists;
   }
-  createListOptions(){
+  createListOptions(expertiseList) {
     let options = [<option key={0}>Select an expertise</option>];
-    this.state.expertises.forEach((key, i) => {
-      options.push(<option key={i+1}>{key}</option>)
+    expertiseList.forEach((key, i) => {
+      options.push(<option key={i + 1}>{key}</option>);
     });
     return options;
   }
-  addExpertiseRow(key, index){
-    if (index === this.state.expSelections.length-1){
+  addExpertiseRow(key, index) {
+    if (index === this.state.expSelections.length - 1) {
       const newArray = this.state.expSelections.concat(['new item']);
       this.setState({
         expertises: this.state.expertises,
-        expSelections: newArray
+        expSelections: newArray,
       });
       this.forceUpdate();
       console.log(this.state.expSelections);
-    };
+    }
   }
 
   /**
    * Final action - send the form
    * @param event
    */
-  handleSubmit(event){
+  handleSubmit(event) {
     event.preventDefault();
+    // eslint-disable-next-line no-undef
     const data = new FormData(event.target).entries();
     let entry;
     const valueArray = [];
-    while ((entry = data.next()).done === false){
-      valueArray.push(entry.value)
-    };
+    while ((entry = data.next()).done === false) {
+      valueArray.push(entry.value);
+    }
     console.log(valueArray);
   }
 
@@ -70,22 +80,37 @@ class ApplicationForm extends Component {
           <Card.Body>
             <Form onSubmit={this.handleSubmit}>
               <h5>Personal information</h5>
-                <div className="row">
-                  <div className="col">
-                    <input type="text" name={'firstName'} className="form-control" placeholder="First name"/>
-                  </div>
-                  <div className="col">
-                    <input type="text" name={'lastName'} className="form-control" placeholder="Last name"/>
-                  </div>
+              <div className="row">
+                <div className="col">
+                  <input
+                    type="text"
+                    name={'firstName'}
+                    className="form-control"
+                    placeholder="First name"
+                  />
                 </div>
-                <input type="email" name={'email'} className="form-control" placeholder="Email adress"/>
-                <h5>Your expertise</h5>
-                <div className="row w-100">
-                  <Form.Group id={'optionsContainer'} className={'w-100'}>
-                    <Form.Label>Example select</Form.Label>
-                    {this.createLists(this.state.expSelections)}
-                  </Form.Group>
+                <div className="col">
+                  <input
+                    type="text"
+                    name={'lastName'}
+                    className="form-control"
+                    placeholder="Last name"
+                  />
                 </div>
+              </div>
+              <input
+                type="email"
+                name={'email'}
+                className="form-control"
+                placeholder="Email adress"
+              />
+              <h5>Your expertise</h5>
+              <div className="row w-100">
+                <Form.Group id={'optionsContainer'} className={'w-100'}>
+                  <Form.Label>Example select</Form.Label>
+                  {this.createLists(this.state)}
+                </Form.Group>
+              </div>
               <button type={'submit'}>Submit</button>
             </Form>
           </Card.Body>
