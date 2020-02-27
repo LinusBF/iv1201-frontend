f8c47d0dfc4e13277c02b33f96f73cb963009b47
Melker Mossberg <melker.mossberg@gmail.com>
2020-02-27 21:50:48 +0100

    on logout user is logged out from firebase,state and redirected

--- client/components/menu/MainMenu.js
+++ client/components/menu/MainMenu.js
@@ -1,28 +1,51 @@
 import React, {Component} from 'react';
 import Logo from './Logo';
 import './MainMenu.css';
+import firebase from '../../firebaseConfig';
+import {connect} from 'react-redux';
+import {logout, setLoginStatus} from '../../redux/actions';
+import {Redirect} from 'react-router-dom';
 
 class MainMenu extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {doLogOut: false};
+    this.logout = this.logout.bind(this);
+  }
+  logout() {
+    const that = this;
+    firebase
+      .auth()
+      .signOut()
+      .then(
+        function() {
+          that.props.logout();
+          that.setState({doLogOut: true});
+        },
+        function(error) {
+          console.log(error);
+        }
+      );
   }
   render() {
     return (
       <div id={'mainMenu'} className={'container'}>
-        <div className="d-flex mt-4">
+        <div className='d-flex mt-4'>
           <div className={'w-100'}>
             <Logo />
           </div>
           <div className={'flex-shrink-0 mt-3'}>
             <div>
-              <a href="#">Log Out</a>
+              <a href='#' onClick={this.logout}>
+                Log Out
+              </a>
             </div>
           </div>
         </div>
+        {this.state.doLogOut === true ? <Redirect to='/' /> : <div />}
       </div>
     );
   }
 }
 
-export default MainMenu;
+export default connect(null, {logout, setLoginStatus})(MainMenu);
